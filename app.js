11201c6e871d28cb3e224ca7c07235da21225e90
DanyGuerra <luis_daniel.grra15@hotmail.com>
2021-09-23 11:43:05 -0500

    Modificando app.js

--- app.js
+++ app.js
@@ -1,33 +1,32 @@
+// Express
 const express = require('express');
 const app = express();
 
-const bodyParser = require('body-parser');
 
+//Body Parser
+const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// Configuracion de la base de datos
+// Configuración de base de datos
 const mongoose = require('mongoose');
+
 mongoose.connect(
   process.env.MONGO_URI, // obtiene la url de conexión desde las variables de entorno
   { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true }
 );
-
-mongoose.set("debug", true);
+mongoose.set("debug", true)
 
 require('./models/Usuario')
 require('./models/Mascota')
-require('./models/Solicitud')
 
-// Al final se importa passport ya que se usan los modelos en la configuracion de passport
-require('./config/passport');
+require('./config/passport')
 
-// Rutas
-app.use('/v1', require('./routes/index'))
+//Rutas
+app.use('/v1', require('./routes'));
 
 // Iniciando el servidor
-const PORT = 4001;
+
 app.listen(process.env.PORT, () => {
   console.log(`Server listening on port ${process.env.PORT}`)
 })
-
